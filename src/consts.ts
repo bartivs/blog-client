5c52d3f61663a8b3be7c3ef3a5e867b1413d5423
bartivs <bartivaldez@gmail.com>
2023-07-30 16:40:22 -0400

    adds: info

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+import type { SocialInfo } from "./types";
+
+export const SITE_TITLE = "Barti's";
+export const AUTHOR = "Barti";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const GITHUB_URL = 'https://github.com/bartivs';
+
+// export const SOCIAL_LINKS: SocialInfo[] = [{
+//     url:"https://github.com/bartivs",astro_icon:"social/github"
+// }, {
+//     url:"https://m.webtoo.ls/@astro",astro_icon:"social/mastodon"
+// }]
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+import { Icon } from "astro-icon"
+export interface SocialInfo {
+    url: string
+    astro_icon:string
+}
