cc303ef332657cd1dad85152c47de1c4adffa571
bartivs <bartivaldez@gmail.com>
2023-08-11 13:40:02 -0400

    client implementation for handling queries

---
+++ src/lib/keystone.ts
@@ -0,0 +1,56 @@
+import type { Post } from "../interfaces/keystone";
+
+interface Props {
+    queryGraphQL?: string;
+}
+
+/**
+ * Fetches data from the Strapi API
+ * @param query - The query parameters to add to the url
+ * @returns
+ */
+export default async function fetchApi({
+    queryGraphQL,
+}: Props): Promise<any>{
+
+    const url = new URL(`${import.meta.env.KEYSTONE_URL}/api/graphql`);
+
+    const res = await fetch(url.toString(), {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            query: queryGraphQL,
+        }),
+    });
+    let { data } = await res.json();
+
+    return data ;
+}
+
+
+export async function getCollection(name: "post" | "tag") {
+    if (name === "post") {
+        const  {posts }  = await fetchApi({
+            queryGraphQL: `
+            query Query {
+                posts {
+                id
+                author {
+                    name
+                }
+                content {
+                    document
+                }
+                title
+                pubDate
+                description
+                } 
+                } 
+      `
+        })
+        return posts as Post[]
+    }
+    if (name === "tag") { 
+        return []
+    }
+}
